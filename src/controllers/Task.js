b270171c1aa266d7b6ee643a1efd72b081760735
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-20 11:48:03 -0300

    feat: create task controller

---
+++ src/controllers/Task.js
@@ -0,0 +1,51 @@
+import { openDb } from "../db_config.js";
+
+export async function createTaskTable() {
+  openDb().then((db) => {
+    db.exec(
+      "CREATE TABLE IF NOT EXISTS tasks ( id INTEGER PRIMARY KEY, title TEXT NOT NULL, description TEXT NOT NULL, status TEXT NOT NULL, dueDate TEXT NOT NULL, teacherId INTEGER NOT NULL, studentId INTEGER NOT NULL, FOREIGN KEY (teacherId) REFERENCES teachers(id), FOREIGN KEY (studentId) REFERENCES students(id) )"
+    );
+  });
+}
+
+export async function insertTask(req, res) {
+  const task = req.body;
+  openDb().then((db) => {
+    db.run(
+      "INSERT INTO tasks (title, description, status, dueDate, teacherId, studentId) VALUES (?,?,?,?,?,?)",
+      [
+        task.title,
+        task.description,
+        task.status,
+        task.dueDate,
+        task.teacherId,
+        task.studentId,
+      ]
+    );
+  });
+  res.json({ message: "Task created successfully!" });
+}
+
+export async function selectTask(req, res) {
+  const id = req.params.id;
+  openDb().then((db) => {
+    db.get("SELECT * FROM tasks WHERE id = ?", [id]).then((task) =>
+      res.json(task)
+    );
+  });
+}
+
+export async function selectTasksByStudentAndTeacherAndStatus(req, res) {
+  const studentId = req.body.studentId;
+  const teacherId = req.body.teacherId;
+  const status = req.body.status; // pendente, completa, corrigida
+  
+  openDb().then((db) => {
+    db.all(`
+      SELECT * FROM tasks
+      WHERE studentId = ? AND teacherId = ? AND status = ?
+    `, [studentId, teacherId, status]).then((tasks) =>
+      res.json(tasks)
+    );
+  });
+}
