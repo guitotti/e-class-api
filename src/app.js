44e27c92cfca5d9173bee68b5c3ff4974978704e
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-25 00:38:22 -0300

    feat: use cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
 import express from "express";
 import fs from "fs/promises";
+import cors from "cors";
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 
 async function checkUploadsFolder() {
   try {
