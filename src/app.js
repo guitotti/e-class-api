3eabbea0df9351f1c26ab6c62978954e471cf04a
Guilherme Totti <dev.guilhermetotti@gmail.com>
2024-11-29 04:07:34 -0300

    feat: cors config

--- src/app.js
+++ src/app.js
@@ -5,6 +5,11 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 const PORT = process.env.PORT || 3000
 
 async function checkUploadsFolder() {
